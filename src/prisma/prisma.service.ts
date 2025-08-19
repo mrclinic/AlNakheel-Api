85d86107d846d311c748ebeff1128ac1765448bb
Mohammad.Mohammad <mohammadabmohammad@gmail.com>
2025-08-19 20:09:34 +0300

    fix prisma issue

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 import { HttpStatus } from '@nestjs/common';
 import { BusinessException } from '../common/exceptions/business.exception';
 
