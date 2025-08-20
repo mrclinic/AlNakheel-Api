ab4262043eb6bca5f4a4870e4d2b30996bb2a420
Mohammad.Mohammad <mohammadabmohammad@gmail.com>
2025-08-20 14:00:39 +0300

    fix register bad request

--- src/modules/auth/dto/auth-credentials.dto.ts
+++ src/modules/auth/dto/auth-credentials.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
-import { IsEmail, IsEnum, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @ApiProperty({ description: 'User Email' })
@@ -13,6 +13,8 @@ export class RegisterDto {
   password: string;
 
   @ApiProperty({ description: 'User name', required: false })
+  @IsOptional()
+  @IsString()
   name?: string;
 
   @ApiProperty({ description: 'User Role - when no role provide --> By Default will be \'ADMIN\'', enum: Role, required: false })
